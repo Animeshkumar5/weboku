f8836c58bf9ef4c40c8a3eb9ab53bc3a60bd8b29
Animesh Kumar <animeshbca003@gmail.com>
2025-12-21 15:08:22 +0530

    fix deployment error

--- src/components/Clients/Clients.jsx
+++ src/components/Clients/Clients.jsx
@@ -57,9 +57,9 @@ import stormImg2 from '../../assets/Client/stormdigi/2.png';
 import stormImg3 from '../../assets/Client/stormdigi/3.png';
 
 // --- UnoTrip ---
-import unoLogo from '../../assets/Client/UnoTrip/1.png';
-import unoImg1 from '../../assets/Client/UnoTrip/2.png';
-import unoImg2 from '../../assets/Client/UnoTrip/3.png';
+import unoLogo from '../../assets/Client/unoTrip/1.png';
+import unoImg1 from '../../assets/Client/unoTrip/2.png';
+import unoImg2 from '../../assets/Client/unoTrip/3.png';
 
 
 // ==========================================
